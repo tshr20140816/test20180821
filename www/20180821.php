9c0b99bd45789df90d898dee2cca7c791f220599
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-08-21 09:09:49 +0900

    Create 20180821.php

---
+++ www/20180821.php
@@ -0,0 +1,29 @@
+<?php
+
+$res = get_contents($_GET['u']);
+
+error_log($res);
+
+function get_contents($url_) {
+  $pid = getmypid();
+  $ch = curl_init();
+  curl_setopt_array($ch,
+                    [CURLOPT_URL => $url_,
+                     CURLOPT_RETURNTRANSFER => TRUE,
+                     CURLOPT_ENCODING => '',
+                     CURLOPT_CONNECTTIMEOUT => 20,
+                     CURLOPT_FOLLOWLOCATION => TRUE,
+                     CURLOPT_MAXREDIRS => 3,
+                     CURLOPT_FILETIME => TRUE,
+                     CURLOPT_PATH_AS_IS => TRUE,
+                     CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/61.0',
+                    ]);
+  @curl_setopt($ch, CURLOPT_TCP_FASTOPEN, TRUE);
+  $contents = curl_exec($ch);
+  
+  curl_close($ch);
+  
+  return $contents;
+}
+
+?>
