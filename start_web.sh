724341b18c52fa5fb65dc4adb61748b2bfdeb136
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-03 08:40:40 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -4,7 +4,7 @@ set -x
 
 export TZ=JST-9
 
-export USER_AGENT=$(curl https://raw.githubusercontent.com/tshr20140816/heroku-mode-06/master/useragent.txt)
+export USER_AGENT=$(curl https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/useragent.txt)
 
 if [ ! -v BASIC_USER ]; then
   echo "Error : BASIC_USER not defined."
