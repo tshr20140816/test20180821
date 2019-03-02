f602584721a2b1cc626002e04f7dce39e223c197
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-03 08:42:05 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -5,5 +5,9 @@ start_date=$(date)
 
 chmod 777 start_web.sh
 
+wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.xz
+
+tar xf aria2-1.34.0.tar.xz
+
 echo ${start_date}
 date
