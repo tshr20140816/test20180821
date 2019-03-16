f9f9af726a793ddce3186f831a984a1858be808b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-17 03:52:54 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -5,6 +5,21 @@ start_date=$(date)
 
 chmod 777 start_web.sh
 
+cd /tmp
+
+wget https://www.samba.org/ftp/ccache/ccache-3.6.tar.xz
+
+tar xf ccache-3.6.tar.xz
+cd ccache-3.6
+
+./configure --help
+./configure --prefix=/tmp/usr
+
+make -j8
+make install
+
+cd ..
+
 wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.xz
 
 tar xf aria2-1.34.0.tar.xz
