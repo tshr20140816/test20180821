b04ab78a258d2566c153b510f615516e20ee57cf
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-17 03:46:21 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -10,10 +10,11 @@ wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.
 tar xf aria2-1.34.0.tar.xz
 cd aria2-1.34.0
 pwd
+./configure --help
 ./configure --prefix=/tmp/usr
-make -j8
-make install
-cp -r /tmp/usr ../usr
+# make -j8
+# make install
+# cp -r /tmp/usr ../usr
 
 echo ${start_date}
 date
