b00c18f66031de248e3e78f24d5927f4ccfc280b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-19 14:36:33 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -236,7 +236,6 @@ time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X DELETE ${WEBDAV_URL}
 time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X PUT ${WEBDAV_URL} \
     -H "Content-Type: application/x-tar" \
     -F "file=@/tmp/ccache_cache.tar.xz"
-cat /tmp/ccache_cache.tar.xz | head -n 3
 ls -lang www/
 
 ccache -s
