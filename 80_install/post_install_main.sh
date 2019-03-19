23866970b2ae28c858fa5e4f88df8b05d2f8fac8
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-19 13:10:52 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+set -x
+
+date
+start_date=$(date)
+
+chmod 777 start_web.sh
+
+pwd
+cd /tmp
+
+curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} ${WEBDAV_URL} -O
+
+ls -lang
+
+unzip -q ccache_cache.zip
+
+ls -lang
+
+tar Jcvf ccache_cache.tar.xz ./ccache_cache
+
+ls -lang
+
+rm -rf ./ccache_cache
+
+ls -lang
+
+tar xf ccache_cache.tar.xz
+
+ls -lang
+
+tree ccache_cache
