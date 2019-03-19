1838e43dcf70095e63668e93c7eb23f6f0e6e34d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-19 14:56:13 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -115,8 +115,11 @@ pushd /tmp
 time tar Jcf ccache_cache.tar.xz ./ccache
 popd
 time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X DELETE ${WEBDAV_URL}
-time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X PUT ${WEBDAV_URL} \
-    -F "file=@/tmp/ccache_cache.tar.xz;type=application/x-compress"
+time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X PUT \
+    -H "Content-Type: application/x-compress" \
+    --data-binary \
+    -F "file=@/tmp/ccache_cache.tar.xz" \
+    ${WEBDAV_URL}
 rm -f /tmp/ccache_cache.tar.xz
 
 wait
@@ -232,8 +235,11 @@ time tar Jcf ccache_cache.tar.xz ./ccache
 ls -lang
 popd
 time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X DELETE ${WEBDAV_URL}
-time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X PUT ${WEBDAV_URL} \
-    -F "file=@/tmp/ccache_cache.tar.xz;type=application/x-compress"
+time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X PUT \
+    -H "Content-Type: application/x-compress" \
+    --data-binary \
+    -F "file=@/tmp/ccache_cache.tar.xz" \
+    ${WEBDAV_URL}
 ls -lang www/
 
 ccache -s
