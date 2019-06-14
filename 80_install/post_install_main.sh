6d45ffe9804a131da3387c27f505e5179aa810dc
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-14 11:25:26 +0900

    Create post_install_main.sh

---
+++ 80_install/post_install_main.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+
+date
+start_date=$(date)
+
+chmod 777 start_web.sh
+
+export PATH="/tmp/usr/bin:${PATH}"
+
+export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
+gcc --version
+g++ -fuse-ld=gold -Wl,--version
+gcc -c -Q -march=native --help=target
+
+grep -c -e processor /proc/cpuinfo
+cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
+
+mkdir -p /tmp/usr/bin
+mkdir -p /tmp/usr/share
+
+wget https://curl.haxx.se/download/curl-7.65.1.tar.xz
+
+cd /tmp
+tar xf curl-7.65.1.tar.xz
+ls -lang
+cd curl
+./configure --help
+
+echo ${start_date}
+date
