9a3e70c4e261f68e595932f1d23876d9e55c500c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-20 14:10:45 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -5,9 +5,24 @@ set -x
 date
 start_date=$(date)
 
+export PATH="/tmp/usr/bin:${PATH}"
+
 export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
+
+mkdir -p /tmp/usr/bin
+cp ccache /tmp/usr/bin/
+chmod +x /tmp/usr/bin/ccache
+
+export CCACHE_DIR=/tmp/ccache
+
+pushd /tmp/usr/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+popd
 
 pushd /tmp
 time git clone https://github.com/meganz/MEGAcmd.git
@@ -16,9 +31,17 @@ time git submodule update --init --recursive
 time sh autogen.sh
 ./configure --help
 time ./configure --prefix=/tmp/usr --disable-curl-checks --enable-static=yes --enable-shared=no
-time make -j2
+time timeout -sKILL 180 make -j2
 popd
 popd
+
+pushd /tmp
+zip -9qr ccache_cache.zip ./ccache
+popd
+time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X DELETE ${WEBDAV_URL}
+time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X PUT ${WEBDAV_URL} -F "file=@/tmp/ccache_cache.zip"
+
+ccache -s
 
 echo ${start_date}
 date
