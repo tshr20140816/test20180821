e4e3385f97e05237c1bcb763af9af7d10ce3982d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-20 18:55:57 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -66,7 +66,7 @@ wait
 ccache -s
 
 # time timeout -sKILL 90 make -j2 | tee /tmp/make_results.txt
-time timeout -sKILL 90 make | tee /tmp/make_results.txt
+time timeout -sKILL 120 make | tee /tmp/make_results.txt
 popd
 popd
 
