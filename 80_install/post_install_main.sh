b5f45c565382c196cc51f82694cc7de72c5e3a08
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-20 14:00:31 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -5,8 +5,20 @@ set -x
 date
 start_date=$(date)
 
-cp Stirling.zip /tmp/Stirling.zip
-time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -X PUT -H "Content-Type: application/x-compress" --data-binary @/tmp/Stirling.zip ${WEBDAV_URL}
+export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+export CXXFLAGS="$CFLAGS"
+export LDFLAGS="-fuse-ld=gold"
+
+pushd /tmp
+time git clone https://github.com/meganz/MEGAcmd.git
+pushd MEGAcmd
+time git submodule update --init --recursive
+time sh autogen.sh
+./configure --help
+time ./configure --prefix=/tmp/usr --disable-curl-checks --enable-static=yes --enable-shared=no
+time make -j2
+popd
+popd
 
 echo ${start_date}
 date
