46d91272136107b324979802f4bd3da4a03dda51
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-21 10:48:05 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -11,18 +11,20 @@ export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS="-fuse-ld=gold"
 
-cd /tmp
+pushd /tmp
 
 time git clone -b v3.7.3 --depth=1 https://github.com/ccache/ccache.git
 
-cd ccache
+pushd ccache
 time sh autogen.sh
 ./configure --help
 time ./configure --prefix=/tmp/usr
 time make -j2
 ls -lang
+popd
+popd
 
-cp ./ccache ~/www/
+cp /tmp/ccache/ccache www/
 
 echo ${start_date}
 date
