1bddd7003ccca91601623e1a90db1473183a29e5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-21 13:24:18 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -25,7 +25,7 @@ export CCACHE_DIR=/tmp/ccache
 # export CCACHE_COMPILERCHECK=content
 export CCACHE_COMPILERCHECK=none
 export CCACHE_LOGFILE=/tmp/ccache.log
-export CCACHE_SLOPPINESS=pch_defines,time_macros
+export CCACHE_SLOPPINESS=pch_defines,time_macros,file_macro
 
 pushd /tmp/usr/bin
 ln -s ccache gcc
