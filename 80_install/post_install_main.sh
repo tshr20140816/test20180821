5bd27be14e6ebee7c269bed13e5550d4c2754f49
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-21 11:58:08 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -37,7 +37,7 @@ ln -s ccache c++
 popd
 
 pushd /tmp
-time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -O ${WEBDAV_URL}ccache_cache.tar.bz2 ${WEBDAV_URL}MEGAcmd.tar.bz2
+time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -O ${WEBDAV_URL}ccache_cache.tar.bz2 -O ${WEBDAV_URL}MEGAcmd.tar.bz2
 # tar xf ccache_cache.tar.bz2 &
 ls -lang
 
