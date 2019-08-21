53ae32c2f58848cce1ae0b0fd7fb1d42fef2ec3b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-21 11:34:26 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -10,6 +10,8 @@ chmod 777 start_web.sh
 pushd /tmp
 # git clone https://github.com/meganz/MEGAcmd.git
 time git clone --recursive --depth=1 --shallow-submodules https://github.com/meganz/MEGAcmd.git
+time tar cf MEGAcmd.tar ./MEGAcmd
+time pbzip2 -p2 --fast MEGAcmd.tar
 ls -lang
 popd
 
