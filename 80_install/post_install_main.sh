345439e462b26c87f7ee57b02d6a015adb21af90
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-22 11:13:05 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -27,13 +27,12 @@ ls -lang /tmp/usr/lib
 curl -O https://curl.haxx.se/download/curl-7.65.3.tar.xz
 tar xf curl-7.65.3.tar.xz
 pushd curl-7.65.3
-# ls -lang
 ./configure --help
 ./configure --prefix=/tmp/usr --enable-shared=no --enable-static=yes \
   --with-libssh2 --with-brotli --with-nghttp2 \
   --with-gssapi --with-libmetalink=/tmp/usr
-# time make -j2
-# make install
+time make -j2
+make install
 popd
 popd
 
