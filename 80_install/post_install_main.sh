427429625b6a4dc136d3021e9fbefcb3006351ed
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-22 11:08:30 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -14,17 +14,17 @@ pushd /tmp
 
 time git clone --depth=1 -b release-0.1.3 https://github.com/metalink-dev/libmetalink.git
 pushd libmetalink
-./buildconf
+time ./buildconf
 ./configure --help
-./configure --prefix=/tmp/usr
-time make
+time ./configure --prefix=/tmp/usr
+time make -j2
 make install
 popd
 
 ls -lang /tmp/usr
 ls -lang /tmp/usr/lib
 
-# curl -O https://curl.haxx.se/download/curl-7.65.3.tar.xz
+curl -O https://curl.haxx.se/download/curl-7.65.3.tar.xz
 tar xf curl-7.65.3.tar.xz
 pushd curl-7.65.3
 # ls -lang
@@ -32,8 +32,8 @@ pushd curl-7.65.3
 ./configure --prefix=/tmp/usr --enable-shared=no --enable-static=yes \
   --with-libssh2 --with-brotli --with-nghttp2 \
   --with-gssapi --with-libmetalink=/tmp/usr/lib
-time make -j2
-make install
+# time make -j2
+# make install
 popd
 popd
 
