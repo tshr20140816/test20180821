600594bec965858c5fdb0f76297643c4944d86f3
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-22 09:43:41 +0900

    Create post_install_main.sh

---
+++ 80_install/post_install_main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+date
+start_date=$(date)
+
+chmod 777 start_web.sh
+
+pbzip2 --help
+megatools --version
+
+pushd /tmp
+curl -O https://curl.haxx.se/download/curl-7.65.3.tar.xz
+tar xf curl-7.65.3.tar.xz
+pushd curl*
+ls -lang
+popd
+popd
+
+echo ${start_date}
+date
