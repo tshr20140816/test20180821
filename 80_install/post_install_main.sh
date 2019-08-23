c00a69f977c2f650db0d681ec35a47353997ac63
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-24 08:48:07 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -10,9 +10,9 @@ chmod 777 start_web.sh
 # pbzip2 --help
 # megatools --version
 
-cat << '__HEREDOC__' | xargs -L1 -P 2 -t -I{}
-curl -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar
-curl -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
+cat << '__HEREDOC__' | xargs -L1 -P 2 -t -I{} curl '{}'
+-O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar
+-O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
 __HEREDOC__
 
 ls -lang
