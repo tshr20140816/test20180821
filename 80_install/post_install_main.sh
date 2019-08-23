1b6647bada1d6d5f034eeb20600e153ab508202c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-23 11:03:13 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -11,7 +11,7 @@ chmod 777 start_web.sh
 
 cat /proc/cpuinfo
 
-export PATH="/tmp/usr/bin:${PATH}"
+# export PATH="/tmp/usr/bin:${PATH}"
 
 # export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 # export CXXFLAGS="$CFLAGS"
@@ -30,12 +30,12 @@ export CCACHE_NOHASHDIR=true
 export CCACHE_DEBUG=true
 # export CCACHE_DEPEND=true
 
-pushd /tmp/usr/bin
-ln -s ccache gcc
-ln -s ccache g++
-ln -s ccache cc
-ln -s ccache c++
-popd
+# pushd /tmp/usr/bin
+# ln -s ccache gcc
+# ln -s ccache g++
+# ln -s ccache cc
+# ln -s ccache c++
+# popd
 
 pushd /tmp
 # time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -O ${WEBDAV_URL}ccache_cache.tar.bz2 -O ${WEBDAV_URL}MEGAcmd.tar.bz2
@@ -61,16 +61,19 @@ ccache -p
 # time timeout -sKILL 90 make -j2 | tee /tmp/make_results.txt
 # export CC='ccache gcc'
 # export CXX='ccache g++'
-time timeout -sKILL 30 make | tee /tmp/make_results.txt
+# time timeout -sKILL 30 make | tee /tmp/make_results.txt
+time make | tee /tmp/make_results.txt
 # ls -lang /tmp/MEGAcmd/sdk/src
 # cat /tmp/MEGAcmd/sdk/src/src_libmega_la-megaclient.o.ccache-input-text
-cat /tmp/MEGAcmd/sdk/src/src_libmega_la-megaclient.o.ccache-log
+# cat /tmp/MEGAcmd/sdk/src/src_libmega_la-megaclient.o.ccache-log
 popd
 popd
 
-ccache -s
+curl ${INFO_URL}finish
 
-wc -l /tmp/make_results.txt
+# ccache -s
+
+# wc -l /tmp/make_results.txt
 
 # pushd /tmp
 # rm -f ccache_cache.tar.bz2
@@ -89,7 +92,7 @@ wc -l /tmp/make_results.txt
 #     --data-binary @/tmp/ccache_cache.tar.bz2 \
 #     ${WEBDAV_URL}ccache_cache.tar.bz2
 
-ccache -s
+# ccache -s
 
 # cat /tmp/ccache.log
 
