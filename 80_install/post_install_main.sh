839a03a091f93916ece494e638bb50048788cbc0
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-23 10:06:07 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -11,15 +11,15 @@ chmod 777 start_web.sh
 
 cat /proc/cpuinfo
 
-# export PATH="/tmp/usr/bin:${PATH}"
+export PATH="/tmp/usr/bin:${PATH}"
 
 # export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
 # export CXXFLAGS="$CFLAGS"
 # export LDFLAGS="-fuse-ld=gold"
 
-# mkdir -p /tmp/usr/bin
-# cp ccache /tmp/usr/bin/
-# chmod +x /tmp/usr/bin/ccache
+mkdir -p /tmp/usr/bin
+cp ccache /tmp/usr/bin/
+chmod +x /tmp/usr/bin/ccache
 
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_COMPILERCHECK=content
@@ -30,19 +30,20 @@ export CCACHE_NOHASHDIR=true
 export CCACHE_DEBUG=true
 # export CCACHE_DEPEND=true
 
-# pushd /tmp/usr/bin
-# ln -s ccache gcc
-# ln -s ccache g++
-# ln -s ccache cc
-# ln -s ccache c++
-# popd
+pushd /tmp/usr/bin
+ln -s ccache gcc
+ln -s ccache g++
+ln -s ccache cc
+ln -s ccache c++
+popd
 
 pushd /tmp
 # time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -O ${WEBDAV_URL}ccache_cache.tar.bz2 -O ${WEBDAV_URL}MEGAcmd.tar.bz2
+time curl -u ${WEBDAV_USER}:${WEBDAV_PASSWORD} -O ${WEBDAV_URL}MEGAcmd.tar.bz2
 # tar xf ccache_cache.tar.bz2 &
-# tar xf MEGAcmd.tar.bz2 &
+tar xf MEGAcmd.tar.bz2 &
 
-time git clone --recursive --depth=1 --shallow-submodules https://github.com/meganz/MEGAcmd.git
+# time git clone --recursive --depth=1 --shallow-submodules https://github.com/meganz/MEGAcmd.git
 
 wait
 
@@ -54,12 +55,12 @@ ccache -z
 ccache -p
 # dir -R ${CCACHE_DIR}
 
-time sh autogen.sh
-time ./configure --prefix=/tmp/usr --disable-curl-checks --enable-static=yes --enable-shared=no
+# time sh autogen.sh
+# time ./configure --prefix=/tmp/usr --disable-curl-checks --enable-static=yes --enable-shared=no
 
 # time timeout -sKILL 90 make -j2 | tee /tmp/make_results.txt
-export CC='ccache gcc'
-export CXX='ccache g++'
+# export CC='ccache gcc'
+# export CXX='ccache g++'
 time timeout -sKILL 30 make | tee /tmp/make_results.txt
 # ls -lang /tmp/MEGAcmd/sdk/src
 # cat /tmp/MEGAcmd/sdk/src/src_libmega_la-megaclient.o.ccache-input-text
