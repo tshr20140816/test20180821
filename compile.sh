9a1bbe58c647577ee69adfc467399a147e180c11
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-23 11:20:12 +0900

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+cat /proc/cpuinfo
+
+# export CFLAGS="-O2 -march=native -mtune=native -fomit-frame-pointer"
+# export CXXFLAGS="$CFLAGS"
+# export LDFLAGS="-fuse-ld=gold"
+
+pushd /tmp
+
+time git clone --recursive --depth=1 --shallow-submodules https://github.com/meganz/MEGAcmd.git
+
+pushd MEGAcmd
+
+time sh autogen.sh
+time ./configure --prefix=/tmp/usr --disable-curl-checks --enable-static=yes --enable-shared=no
+
+time make -j8 1>&2
+popd
+popd
