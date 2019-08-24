aeed1b05c3fc612c8ca21831e5b927eb5dff810d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-24 09:09:17 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -11,8 +11,9 @@ chmod 777 start_web.sh
 # megatools --version
 
 cat << '__HEREDOC__' >jobs.txt
-curl -v -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar
-curl -v -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
+curl -v --compressed -O https://oscdl.ipa.go.jp/IPAexfont/ipaexg00401.zip
+curl -v --compressed -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar
+curl -v --compressed -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
 __HEREDOC__
 
 cat jobs.txt | parallel -j2
