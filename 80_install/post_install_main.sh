621d0cfe9b0e0e74fbbe67c828c835e715b07e54
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-26 00:04:20 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -12,10 +12,19 @@ chmod 777 start_web.sh
 
 pushd /tmp
 curl -o nautilus-dropbox.tar.bz2 https://linux.dropbox.com/packages/nautilus-dropbox-2019.02.14.tar.bz2
-tar xf nautilus-dropbox.tar.bz2
+mkdir nautilus-dropbox
+tar xf nautilus-dropbox.tar.bz2 -C nautilus-dropbox --strip-components 1
 ls -lang
-
+pushd nautilus-dropbox
+./configure --help
+./configure --prefix=/tmp/usr
+time make
+time make install
 popd
+popd
+
+ls -lang /tmp/usr
+ls -lang /tmp/usr/bin
 
 echo ${start_date}
 date
