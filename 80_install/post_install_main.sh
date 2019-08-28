9ad228dd0485ef48522dd568f957b965429ea0ab
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-28 13:53:49 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -10,16 +10,14 @@ chmod 777 start_web.sh
 # pbzip2 --help
 # megatools --version
 
-pushd /tmp
+mkdir dropbox
+pushd dropbox
 curl -O https://linux.dropbox.com/packages/dropbox.py
 chmod +x dropbox.py
 time timeout -sKILL 30 ./dropbox.py help
 time timeout -sKILL 30 ./dropbox.py version
 time timeout -sKILL 30 ./dropbox.py update
 popd
-
-ls -lang /tmp/usr
-ls -lang /tmp/usr/bin
 
 echo ${start_date}
 date
