adadcd7c90ee0ecbc0ab8786cca674c9253f4e44
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-29 10:30:02 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -16,7 +16,7 @@ curl -O https://linux.dropbox.com/packages/dropbox.py
 chmod +x dropbox.py
 time timeout -sKILL 30 ./dropbox.py help
 time timeout -sKILL 30 ./dropbox.py version
-time timeout -sKILL 30 echo y|./dropbox.py update
+# time timeout -sKILL 30 echo y|./dropbox.py update
 # ./dropbox.py start
 # ./dropbox.py lansync n
 # ./dropbox.py status
