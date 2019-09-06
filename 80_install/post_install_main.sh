cf62f278ed8dd970eba04531d4c1e08241e442a0
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-06 14:50:46 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -11,20 +11,20 @@ chmod 777 start_web.sh
 # megatools --version
 
 cat << '__HEREDOC__' >jobs.txt
-curl -v --compressed -O https://oscdl.ipa.go.jp/IPAexfont/ipaexg00401.zip
-curl -v --compressed -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar
-curl -v --compressed -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
+curl -v -O https://oscdl.ipa.go.jp/IPAexfont/ipaexg00401.zip
+curl -v -L -o migu-1m.zip "https://ja.osdn.net/frs/redir.php?m=iij&f=mix-mplus-ipa/63545/migu-1m-20150712.zip"
+curl -v -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar
+curl -v -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
 __HEREDOC__
 
-# time cat jobs.txt | parallel -j2
+time cat jobs.txt | parallel -j2
 
 # time curl -v -L -o migu-1m-20150712.zip "https://ja.osdn.net/frs/redir.php?m=iij&f=mix-mplus-ipa/63545/migu-1m-20150712.zip"
 
-time curl -sS -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar \
-              -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar \
-              -O https://oscdl.ipa.go.jp/IPAexfont/ipaexg00401.zip \
-              -L -o migu-1m.zip "https://ja.osdn.net/frs/redir.php?m=iij&f=mix-mplus-ipa/63545/migu-1m-20150712.zip"
-
+# time curl -sS -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar \
+#               -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar \
+#               -O https://oscdl.ipa.go.jp/IPAexfont/ipaexg00401.zip \
+#               -L -o migu-1m.zip "https://ja.osdn.net/frs/redir.php?m=iij&f=mix-mplus-ipa/63545/migu-1m-20150712.zip"
 
 ls -lang
 time unzip migu-1m.zip
