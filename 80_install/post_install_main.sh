341d492694ec0c7459d08a577f52b69f2f107d11
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-06 14:53:57 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -17,7 +17,7 @@ curl -v -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/
 curl -v -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
 __HEREDOC__
 
-time cat jobs.txt | parallel -j2
+time cat jobs.txt | parallel -j2 --joblog /tmp/joblog.txt 2>&1
 
 # time curl -v -L -o migu-1m-20150712.zip "https://ja.osdn.net/frs/redir.php?m=iij&f=mix-mplus-ipa/63545/migu-1m-20150712.zip"
 
@@ -25,6 +25,8 @@ time cat jobs.txt | parallel -j2
 #               -O https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar \
 #               -O https://oscdl.ipa.go.jp/IPAexfont/ipaexg00401.zip \
 #               -L -o migu-1m.zip "https://ja.osdn.net/frs/redir.php?m=iij&f=mix-mplus-ipa/63545/migu-1m-20150712.zip"
+
+cat /tmp/joblog.txt
 
 ls -lang
 time unzip migu-1m.zip
