4ca0a5b8e8ff77bedcd99eec2b5bb314af1cf836
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-09-06 14:28:21 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -18,6 +18,10 @@ __HEREDOC__
 
 # time cat jobs.txt | parallel -j2
 
+curl -v -o migu-1m-20150712.zip https://ja.osdn.net/frs/redir.php?m=iij&f=mix-mplus-ipa%2F63545%2Fmigu-1m-20150712.zip
+
+ls -lang
+unzip migu-1m-20150712.zip
 ls -lang
 
 echo ${start_date}
